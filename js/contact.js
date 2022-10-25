e0511acc657196d9613f39f1b435e01b89c5b1aa
z20axa <alejandrospan@hotmail.com>
2022-10-24 20:05:50 -0400

    Revised the map coordinates for the address in the contact js file

--- js/contact.js
+++ js/contact.js
@@ -36,9 +36,9 @@ textAreaEl.addEventListener("change", changeHandler);
 
 // });
 
-var map = L.map('map').setView([39.95605, -75.21207],13);
+var map = L.map('map').setView([39.9382, -75.1723],13);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([39.95605, -75.21207]).addTo(map);
+var marker = L.marker([39.9382, -75.1723]).addTo(map);
